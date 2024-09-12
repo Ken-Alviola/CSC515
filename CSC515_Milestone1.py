0a8656609222c48d38d5672c25c3782403bdcc2b
Ken <kenalviola@gmail.com>
2024-09-12 13:26:11 -0500

    removed comment

--- CSC515_Milestone1.py
+++ CSC515_Milestone1.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import cv2
-
-
-# In[3]:
-
 
 img = cv2.imread('brain.jpg')
 cv2.imwrite('brain_copy.png',img)
@@ -17,7 +11,6 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# In[ ]:
 
 
 
